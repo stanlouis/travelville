647a993168ab7d703fb480fbba2b7404f18afee1
Stanley Louis <stanlouis@msn.com>
2018-09-16 08:17:59 -0400

    Add Search with autocomplete

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
 $(document).ready(function() {
+  // Init Side nav
   $('.sidenav').sidenav();
+
+  // Init Slider
   $('.slider').slider({
     indicators: false,
     height: 500,
     transition: 500,
     interval: 6000
+  });
+
+  // Autocomplete
+  $('input.autocomplete').autocomplete({
+    data: {
+      aruba: null,
+      'Cancun Mexico': null,
+      Hawaii: null,
+      Florida: null,
+      California: null,
+      Jamaica: null,
+      Europe: null,
+      'The Bahamas': null
+    }
   });
 });
